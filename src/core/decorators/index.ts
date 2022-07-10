f2e038705ea0c4b5589554888b2941c055ab10ef
matheushas <matheushenrique.ads@gmail.com>
2022-07-10 12:53:09 -0300

    chore: update index.ts export

--- src/core/decorators/index.ts
+++ src/core/decorators/index.ts
@@ -2,3 +2,5 @@ export * from './controller.decorator';
 export * from './module.decorator';
 export * from './get.decorator';
 export * from './post.decorator';
+export * from './put.decorator';
+export * from './delete.decorator';
