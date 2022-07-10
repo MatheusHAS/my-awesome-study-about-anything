a2c4c9fd84f43084fc4d3ea7a1376dbe919ac46a
matheushas <matheushenrique.ads@gmail.com>
2022-07-10 12:53:32 -0300

    fix: update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,15 @@ export const routePathSanitize = (path: string) => {
   }
   return `/${cleanPath}`;
 };
+
+export const Logger = (context: any, message: any) => {
+  let contextFormatted = context;
+  if (typeof context === 'function') {
+    contextFormatted = getClassName(context);
+  }
+  console.log(`[${contextFormatted}]: ${message}`);
+};
+
+export const getClassName = (classElement: any) => {
+  return classElement.toString().match(/ (\w+)/)[1];
+};
