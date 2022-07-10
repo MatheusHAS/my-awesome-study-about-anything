99cd68f48e17b8e397a8a17741ad9e51f351eb26
matheushas <matheushenrique.ads@gmail.com>
2022-07-10 12:53:55 -0300

    fix: update start server example

--- src/example-app/index.ts
+++ src/example-app/index.ts
@@ -1,10 +1,12 @@
+import { json, urlencoded } from 'express';
 import 'reflect-metadata';
 import Server from 'src/application/express';
 import RootModule from 'src/example-app/root.module';
 
 (async () => {
   const PORT = process.env.PORT || 3000;
-  const application = new Server(PORT);
+  const middlewares = [json(), urlencoded()];
+  const application = new Server(PORT, middlewares);
 
   application.start(() => {
     console.log(`Listening server on ${PORT} port`);
