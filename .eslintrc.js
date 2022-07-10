b8c522d1e8ff70635c9cbad91e1c3a0163087a61
matheushas <matheushenrique.ads@gmail.com>
2022-07-09 23:52:57 -0300

    chore: start configs

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  tabSize: 2,
+};
